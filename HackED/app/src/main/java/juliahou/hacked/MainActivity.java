564aa68bf2bd20f60909a95ee820a805011fd264
juliahou8 <jjhou@andrew.cmu.edu>
2017-10-28 10:32:40 -0400

    Initializing

---
+++ HackED/app/src/main/java/juliahou/hacked/MainActivity.java
@@ -0,0 +1,13 @@
+package juliahou.hacked;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
